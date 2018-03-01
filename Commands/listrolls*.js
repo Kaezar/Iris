251250480d81f0dbebb8f14a8668645bbd5274ed
Kaezar <rokki9@gmail.com>
2018-03-01 13:11:34 -0600

    added listrolls* command

---
+++ Commands/listrolls*.js
@@ -0,0 +1,17 @@
+const iris = require('../iris.js');
+const Rolls = require('../dbObjects.js');
+module.exports = {
+    name: 'listrolls*',
+    description: 'Lists all custom rolls for every user.',
+    kyleOnly: true,
+    execute(message) {
+		Rolls.findAll({ attributes: ['user_name', 'name', 'roll'] })
+        	.then((rollList) => {
+        		const rollString = rollList.map(t => `${t.user_name}.${t.name}: ${t.roll}`).join('\n') || 'No rolls set.';
+        		return message.channel.send(iris.wrap(rollString));
+        	})
+        	.catch((error) => {
+				console.log(error);
+			});
+    },
+};
