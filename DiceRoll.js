00733cd3f796ab25957d873492070d0d0ea334e1
Kyle <rokki9@gmail.com>
2018-04-25 11:39:47 -0500

    added docs for DiceRoll

--- DiceRoll.js
+++ DiceRoll.js
@@ -3,11 +3,19 @@
  * @module DiceRoll
  */
 module.exports = class DiceRoll {
+    // count is the number of dice, and sides is the number of sides each die has.
+    // mod is a number to add to the result after rolling all dice, or undefined if
+    // no mod is given.
     constructor(count, sides, mod) {
         this.count = count;
         this.sides = sides;
         this.mod = mod || undefined;
     }
+    /**
+     * roll a number of dice equal to count, with a number of sides equal to sides
+     * then if there is a mod, add it to the result.
+     * @return {number} result of the roll
+     */
     roll() {
         let result = 0;
 		for(let i = 0; i < this.count; i++) {
